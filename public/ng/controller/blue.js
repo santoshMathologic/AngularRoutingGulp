bb5cdabcfa3455cd4bbd88f4ae8b47352110440d
rakesh <rak1994kumar@gmail.com>
2016-11-04 13:42:48 -0700

    controller added

---
+++ public/ng/controller/blue.js
@@ -0,0 +1,14 @@
+/**
+ * Created by SANTOSH on 11/2/2016.
+ */
+'use strict';
+
+angular.module('smartApp')
+    .controller('blueCtrl', function($scope,$http) {
+
+ 
+ $scope.message = "Inside Blue Controller"
+
+
+
+    });
---
+++ public/ng/directives/customValidation.js
@@ -0,0 +1,16 @@
+angular.module('smartApp').directive('myDirective', function() {
+    return {
+        require: 'ngModel',
+        link: function(scope, element, attr, mCtrl) {
+            function myValidation(value) {
+                if (value.indexOf("e") > -1) {
+                    mCtrl.$setValidity('charE', true);
+                } else {
+                    mCtrl.$setValidity('charE', false);
+                }
+                return value;
+            }
+            mCtrl.$parsers.push(myValidation);
+        }
+    };
+});
