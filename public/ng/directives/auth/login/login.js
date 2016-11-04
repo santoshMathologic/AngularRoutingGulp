0bd8c42ae56beea7fe5b5309719acea90aa7cba6
rakesh <rak1994kumar@gmail.com>
2016-11-04 15:33:24 -0700

    Gulp and font awesome added

--- public/ng/directives/auth/login/login.js
+++ public/ng/directives/auth/login/login.js
@@ -1,14 +1,27 @@
 angular.module('smartApp')
-    .directive('login', ['$compile', function($compile) {
+    .directive('login', ['$compile', function ($compile) {
         return {
             restrict: 'E',
             templateUrl: 'ng/directives/auth/login/login.tmpl.html',
-            controller: function($scope, $state, $window, $location) {
-               
-            
-        
-    
-}
+            controller: function ($scope, $state, $window, $location) {
+
+
+                $scope.userObject = {
+                    username: "",
+                    password: ""
+                };
+
+                $scope.doLogin = function () {
+
+                    $state.go('home.dashboard.user');
+
+
+                }
+
+
+
+
+            }
 
 
 
